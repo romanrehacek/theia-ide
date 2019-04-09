b67796ee7a78f8262c4b000cf0bb53fe57146054
Roman Rehacek <roman@rehacek.sk>
2019-04-09 11:19:35 +0200

    Add settings file

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
-docker run -it \
+docker run -d -it \
     -p 3000:3000 \
+    --restart always \
     --name my-theia \
     -v "/var/www:/home/project:cached" \
+    -v "$(pwd)/settings.json:/home/theia/.theia/settings.json" \
     my_theia
 
